efb8d0e8af7b38d4e7130a6492953fc5010c24bd
jae900 <jae900>
2018-10-01 16:35:48 +0900

    간드앙!

---
+++ app/src/main/java/com/example/user/myapplication/twoButtons.java
@@ -0,0 +1,13 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class twoButtons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_buttons);
+    }
+}
